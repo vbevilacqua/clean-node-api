e8c3d1e22319320f6b0208d496435bc355d75b9b
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-05-29 20:02:23 +0200

    move errors to a custom folder

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,5 @@
 const LoginRouter = require('./login-router')
-const InvalidParamError = require('../helpers/invalid-param-error')
-const MissingParamError = require('../helpers/missing-param-error')
-const UnauthorizedError = require('../helpers/unauthorized-error')
-const ServerError = require('../helpers/server-error')
+const { InvalidParamError, MissingParamError, ServerError, UnauthorizedError } = require('../errors')
 
 const makeSut = () => {
   // Mocked class in order to test
