ea91d560b8164df945ea07084c32bf547145d5fb
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-05-29 15:40:48 +0200

    ensure LoginRouter returns Unauthorized Error and move error 401 to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -16,8 +16,6 @@ module.exports = class LoginRouter {
       return HttpResponse.badRequest('password')
     }
     this.authUseCase.auth(email, password)
-    return {
-      status: 401
-    }
+    return HttpResponse.unauthorizedError()
   }
 }
