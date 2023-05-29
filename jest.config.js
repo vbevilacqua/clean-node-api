ce2c281e709038346e53b0b2e5ce4b1dd566f36f
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-05-29 19:11:29 +0200

    add jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
-  preset: '@shelf/jest-mongodb'
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**']
 }
