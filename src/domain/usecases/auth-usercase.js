c1094edd91fbb3fe2a94f1a0f2eb7c6e759c929a
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-05-30 11:10:00 +0200

    ensure AuthUseCase calls Encrypter with correct values

---
+++ src/domain/usecases/auth-usercase.js
@@ -0,0 +1,25 @@
+const { MissingParamError } = require('../../utils/errors')
+
+module.exports = class AuthUseCase {
+  constructor (loadUserByEmailRepository, encrypter) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+    this.encrypter = encrypter
+  }
+
+  async auth (email, password) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
+    await this.encrypter.compare(password, user.password)
+    return null
+  }
+}
