9cedeabaac41d7d2d3fe12d0747653692865b09f
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-05-30 13:30:07 +0200

    ensure AuthUseCase returns an accessToken if valid credentials are provided

--- src/domain/usecases/auth-usercase.js
+++ src/domain/usecases/auth-usercase.js
@@ -25,6 +25,7 @@ module.exports = class AuthUseCase {
       return null
     }
 
-    await this.tokenGenerator.generate(user.id)
+    const accessToken = await this.tokenGenerator.generate(user.id)
+    return accessToken
   }
 }
